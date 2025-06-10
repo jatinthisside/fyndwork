35ca3dc981c9c19809e2547c0d96076f153bd66c
jatinthisside <jatinkyadu@gmail.com>
2025-06-10 20:32:25 +0530

    feat: add rate limitting middleware for otp

--- server/src/middlewares/rateLimitter.ts
+++ server/src/middlewares/rateLimitter.ts
@@ -5,3 +5,9 @@ export const authLimiter = rateLimit({
   max: 50, // Limit each IP to 50 requests per windowMs
   message: { message: 'Too many requests, please try again later.' },
 });
+
+export const otpRateLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 min
+  max: 3, // 3 OTP requests per window
+  message: 'Too many OTP requests, try again later',
+});
