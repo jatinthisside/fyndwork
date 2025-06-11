551435557fbb46eafcf7381699526b59569f893b
jatinthisside <jatinkyadu@gmail.com>
2025-06-11 11:49:10 +0530

    feat: setup cors policy for cross origin requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,10 +4,19 @@ import helmet from 'helmet';
 import routes from './routes';
 import { performanceLogger } from './middlewares/performanceLogger';
 import { errorHandler } from './middlewares/errorHandler';
+import { CLIENT_ORIGIN } from './config';
 
 const app = express();
 
-app.use(cors()); 
+app.use(
+    cors({
+      origin: CLIENT_ORIGIN,
+      credentials: true, // allow cookies (like accessToken)
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+); 
+
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
