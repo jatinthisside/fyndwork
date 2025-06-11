afb30dc19acb21425837f039478387dc3b781a41
jatinthisside <jatinkyadu@gmail.com>
2025-06-11 11:50:40 +0530

    setup: add new import for env variavle - NODE_ENV

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -6,8 +6,11 @@ dotenv.config();
 export const {
   PORT = '3000',
   MONGO_URI = 'mongodb://localhost:27017/myapp',
-  JWT_SECRET = 'supersecretkey',
-  JWT_EXPIRES = '1d',
+  JWT_SECRET,
+  JWT_EXPIRES,
+  NODE_ENV = 'development',
   CLIENT_ORIGIN = 'http://localhost:3000',
   REDIS_URL = 'redis://localhost:6379',
 } = process.env;
+
+
