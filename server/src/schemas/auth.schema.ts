ad233be04cc3ddf4a44e679110f0c0992043519a
jatinthisside <jatinkyadu@gmail.com>
2025-06-12 19:42:19 +0530

    fix: update signup validation schema for more strict validation

--- server/src/schemas/auth.schema.ts
+++ server/src/schemas/auth.schema.ts
@@ -4,14 +4,14 @@ export const signupSchema = z.object({
     name: z.string({required_error:"name is required", invalid_type_error:"name must be a valid string"}).min(3, "Name must be atleast 3 characters long"),
     email: z.string({required_error:"email is required"}).email("Invalid email format"),
     role: z.enum(['student', 'company', 'admin']),
-    about: z.string({invalid_type_error:"about should be a valid string"}).optional(),
-    city: z.string({invalid_type_error:"city should be a valid string"}).optional(),
-    state: z.string({invalid_type_error:"state should be a valid string"}).optional(),
-    country: z.string({invalid_type_error:"country should be a valid string"}).optional(),
-    pincode: z.number({invalid_type_error:"pincode should be a valid number"}).optional(),
-    street: z.string({invalid_type_error:"street address should be a valid string"}).optional(),
-    password: z.string({invalid_type_error:"password should be a valid string"}).optional(), // Optional for OAuth users
-    profile_photo: z.string({invalid_type_error:"profile_photo should be a valid string"}).optional(),
+    about: z.string({invalid_type_error:"about should be a valid string", required_error:"about is required"}),
+    city: z.string({invalid_type_error:"city should be a valid string",required_error:"city is required"}),
+    state: z.string({invalid_type_error:"state should be a valid string",required_error:"state is required"}),
+    country: z.string({invalid_type_error:"country should be a valid string",required_error:"country is required"}),
+    pincode: z.number({invalid_type_error:"pincode should be a valid number",required_error:"pincode is required"}),
+    street: z.string({invalid_type_error:"street address should be a valid string",required_error:"street is required"}),
+    password: z.string({invalid_type_error:"password should be a valid string",required_error:"password is required"}).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, "Password must be at least 6 characters long, contain at least one uppercase letter, one lowercase letter, and one number"),
+    profile_photo: z.string({invalid_type_error:"profile_photo should be a valid string",required_error:"profile_photo is required"}),
 });
 
 export const loginSchema = z.object({
