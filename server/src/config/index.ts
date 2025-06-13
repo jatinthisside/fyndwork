961f756f42c7a64e44b47f15279896728c5b1c13
jatinthisside <jatinkyadu@gmail.com>
2025-06-13 18:12:02 +0530

    config: configure cloudinary instance

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,6 +2,7 @@ import {logger} from "./logger"
 import { connectToDB  } from "./db"
 import { redisClient } from "./redis";
 import { JWT_SECRET, JWT_EXPIRES, CLIENT_ORIGIN,NODE_ENV,CLOUDINARY_CLOUD_NAME,CLOUDINARY_API_KEY,CLOUDINARY_API_SECRET } from './env';
+import { cloudinary } from "./cloudinary";
 
-export { logger, connectToDB, redisClient, JWT_SECRET, JWT_EXPIRES, CLIENT_ORIGIN,NODE_ENV, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME };
+export { logger, connectToDB, redisClient, JWT_SECRET, JWT_EXPIRES, CLIENT_ORIGIN,NODE_ENV, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME, cloudinary };
 
