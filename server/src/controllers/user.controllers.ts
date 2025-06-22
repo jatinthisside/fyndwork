ef24e4dc94b8edacaee0ab1d3f508efba5198ec3
jatinthisside <jatinkyadu@gmail.com>
2025-06-22 11:38:27 +0530

    feat: add api to create or update student basic profile

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -1,4 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
+import { StudentProfile } from "../models";
+import { toObjectId } from "../utils";
+import { IStudentProfile } from "../types";
+import { StatusCodes } from 'http-status-codes';
 
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -8,3 +12,33 @@ export const getAllUsers = async (req: Request, res: Response, next: NextFunctio
     next(err);
   }
 };
+
+export const setupStudentProfile=async(req: Request | any, res: Request | any)=>{
+  try {
+    const userId = req.user!.user_id;
+
+    const { skills, portfolio, resume, dob } = req.body;
+
+    const profileData: Partial<IStudentProfile> = {
+      user: toObjectId(userId),
+      skills: skills,
+      portfolio: portfolio,
+      resume: resume,
+      dob: dob,
+    };
+
+    const profile = await StudentProfile.findOneAndUpdate(
+      { user: userId },
+      profileData,
+      { new: true, upsert: true, runValidators: true }
+    );
+
+    res.status(StatusCodes.ACCEPTED).json({
+      success: true,
+      message: 'Student profile updated successfully',
+      profile
+    });
+  } catch (error:any) {
+     throw new Error(error.message || 'Error setting up student profile');
+  }
+}
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { getAllUsers,setupStudentProfile } from '../controllers/user.controllers';
 import { authenticate } from '../middlewares/auth';
+import { validateRequest } from '../middlewares/validateRequest';
+import { studentProfileSchema } from '../schemas/profile.schema';
 
 const router = Router();
 
 router.get('/', getAllUsers); // GET /api/v1/users
-router.post('/profile/student', authenticate("all") , setupStudentProfile);
+router.post('/profile/student', authenticate("student"), validateRequest(studentProfileSchema), setupStudentProfile);
 
 export default router;
