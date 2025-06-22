c56f5b2e28f9eb56022ef03edbc2945b495b60a6
jatinthisside <jatinkyadu@gmail.com>
2025-06-22 13:08:09 +0530

    feat: add api route to delete student-account

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
-import { StudentProfile } from "../models";
+import { StudentProfile, Education, Certificate, Task, User } from "../models";
 import { toObjectId } from "../utils";
 import { IStudentProfile } from "../types";
 import { StatusCodes } from 'http-status-codes';
 import { redisClient } from '../config';
+import mongoose from 'mongoose';
 
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -53,11 +54,11 @@ const PROFILE_CACHE_TTL = 60 * 60;
 
 export const getStudentProfile = async (req: Request, res: Response):Promise<any> => {
   try {
-    if (!req.user || !req.user._id) {
+    if (!req.user || !req.user.user_id) {
       return res.status(StatusCodes.FORBIDDEN).json({success:false, error: "Unauthorized - Invalid user token" });
     }
 
-    const userId = req.user._id;
+    const userId = req.user.user_id;
     const cacheKey = `profile:${userId}`;
 
     const cachedProfile = await redisClient.get(cacheKey);
@@ -69,7 +70,7 @@ export const getStudentProfile = async (req: Request, res: Response):Promise<any
       });
     }
 
-    const profile = await StudentProfile.findOne({ user: userId })
+    const profile = await StudentProfile.findOne({ user: userId, is_deleted: false })
       .populate({
         path: "user",
         select: "name email profile_photo role about city state country pincode street dob",
@@ -108,5 +109,66 @@ export const getStudentProfile = async (req: Request, res: Response):Promise<any
     });
   } catch (error: any) {
     throw new Error(error.message)
+  }
+};
+
+export const deleteStudentAccount = async (req: Request, res: Response) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  
+  try {
+    const userId = req.user!.user_id;
+
+    if(!userId) {
+      return res.status(StatusCodes.FORBIDDEN).json({success:false, error: "Unauthorized - Invalid user token" });
+    }
+
+    const user = await User.findOne({_id:userId, is_deleted: false }).session(session);
+
+    if(!user) {
+      return res.status(StatusCodes.NOT_FOUND).json({success:false, error: "User not found" });
+    }
+
+    await Promise.all([
+      User.updateOne({ _id: userId },{ $set : {is_deleted:true}}).session(session),
+      StudentProfile.updateOne({ user: userId },{ $set:{is_deleted:true}}).session(session),
+      
+      Education.updateMany({ user: userId },{ $set:{ is_deleted:true }}).session(session),
+      Certificate.updateMany({ student: userId },{ $set:{ is_deleted:true }}).session(session),
+
+      Task.updateMany(
+        { 
+          $or: [
+            { 'applicants': userId },
+            { 'saved_by': userId },
+            { 'completed_by': userId }
+          ]
+        },
+        { 
+          $pull: { 
+            applicants: userId,
+            saved_by: userId,
+            completed_by: userId 
+          }
+        }
+      ).session(session)
+    ]);
+
+    await redisClient.del(`user:${userId}`);
+    await redisClient.del(`profile:${userId}`);
+
+    await session.commitTransaction();
+
+    res.clearCookie('token');
+    
+    res.status(StatusCodes.ACCEPTED).json({
+      success: true,
+      message: "Account deleted successfully"
+    });
+  } catch (error:any) {
+    await session.abortTransaction();
+    throw new Error(error.message || 'Error while deleting account');
+  } finally {
+    session.endSession();
   }
 };
