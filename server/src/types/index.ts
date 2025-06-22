b5a1d0ee468ee70b313ab393227c2f6de76d92da
jatinthisside <jatinkyadu@gmail.com>
2025-06-22 10:43:33 +0530

    fix: resolve incorrect import statement for middleware type

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,5 +1,5 @@
 import { signupData, loginData } from "./auth.types";
 import {IEducation, IStudentProfile} from "./profile.types";
-import { IUser } from "./user.types";
+import { IUser } from "./middleware.types";
 
 export { signupData, loginData, IEducation, IStudentProfile, IUser };
